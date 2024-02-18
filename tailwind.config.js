daacd99dabd3a1c02eb96aa0c5940355e4531cbd
mschz <m4itesanch3z@gmail.com>
2024-02-18 12:10:40 -0300

    Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 const withMT = require("@material-tailwind/react/utils/withMT");
- 
+const { typography } = require('@material-tailwind/react');
+
 module.exports = withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  plugins: [
+  ],
   theme: {
-    extend: {},
+    fontFamily: { 
+      sans: ['Montserrat', 'sans-serif']
+      } 
   },
-  plugins: [],
 })
