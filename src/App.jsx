a6a1e3ca4644bc65ecc86f0bff33806f2a25cfb9
irinag00 <irinagorlino@gmail.com>
2024-02-23 15:14:42 -0300

    arreglos de diseño

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import { StickyNavbar } from "./Components/Navbar";
+import ServiciosSection from "./sections/ServiciosSection";
 
 function App() {
   return (
     <div className="App">
       <StickyNavbar />
+      <ServiciosSection />
     </div>
   );
 }
