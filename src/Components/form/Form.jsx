1a8b5b9bdf0a55a1872c99569775d918cf251e14
mschz <m4itesanch3z@gmail.com>
2024-02-29 17:08:29 -0300

    id contacto

--- src/Components/form/Form.jsx
+++ src/Components/form/Form.jsx
@@ -27,7 +27,7 @@ import {
       }
     };
     return (
-<section className="text-gray-600 body-font relative bg-darkBlue">
+<section className="text-gray-600 body-font relative bg-darkBlue" id="contacto">
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-col text-center w-full mb-12">
       <Typography
