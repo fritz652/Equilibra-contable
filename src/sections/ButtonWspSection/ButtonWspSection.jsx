351d7e51fa3ede5cff19f19b0208332fd61c5933
irinag00 <irinagorlino@gmail.com>
2024-03-04 11:26:55 -0300

    modificacion switch

--- src/sections/ButtonWspSection/ButtonWspSection.jsx
+++ src/sections/ButtonWspSection/ButtonWspSection.jsx
@@ -1,14 +1,14 @@
-import FooterData from "../../data/FooterData.json"; 
-import "./ButtonWspSection.css"
+import FooterData from "../../data/FooterData.json";
+import "./ButtonWspSection.css";
 
 const ButtonWspSection = () => {
-   const {wsSection } = FooterData;
-   const {href} = wsSection[0];
+  const { wsSection } = FooterData;
+  const { href } = wsSection[0];
   return (
     <a href={href} className="btn-wsp" target="_blank">
       <i className="fa fa-whatsapp icono"></i>
     </a>
-  )
-}
+  );
+};
 
-export default ButtonWspSection
+export default ButtonWspSection;
